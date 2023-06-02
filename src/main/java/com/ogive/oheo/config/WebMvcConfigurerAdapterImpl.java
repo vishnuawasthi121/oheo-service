0316a773668b76ed86900e8bfbcd34a4e4e741fc
vishnuawasthi121 <vishnuawasthi121@gmail.com>
2023-06-02 16:26:07 +0530

    .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",                  "Access-Control-Request-Headers")
             .exposedHeaders("Access-Control-Allow-Origin",
    "Access-Control-Allow-Credentials")

--- src/main/java/com/ogive/oheo/config/WebMvcConfigurerAdapterImpl.java
+++ src/main/java/com/ogive/oheo/config/WebMvcConfigurerAdapterImpl.java
@@ -26,7 +26,12 @@ public class WebMvcConfigurerAdapterImpl extends WebMvcConfigurerAdapter {
 		registry.addMapping("/**")
 		 .allowedOrigins("*")
          //.allowedMethods("GET","POST","PUT", "DELETE","FETCH","OPTIONS")
-		 .allowedMethods("*")
-         .allowedHeaders("*");
+		 
+		 .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                 "Access-Control-Request-Headers")
+		 
+         .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+         
+		 .allowedMethods("*");
 	}
 }
