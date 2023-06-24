236ca5cdeba8c3046151ada672cd056d87b85450
vishnuawasthi121 <vishnuawasthi121@gmail.com>
2023-06-24 19:16:02 +0530

    @Modifying in spec and feature method

--- src/main/java/com/ogive/oheo/persistence/repo/FeaturesRepository.java
+++ src/main/java/com/ogive/oheo/persistence/repo/FeaturesRepository.java
@@ -1,9 +1,12 @@
 package com.ogive.oheo.persistence.repo;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
 
 import com.ogive.oheo.persistence.entities.Features;
 
 public interface FeaturesRepository extends CrudRepository<Features, Long> {
+	
+	@Modifying
 	void deleteByProductId(long productId);
 }
--- src/main/java/com/ogive/oheo/persistence/repo/ProductSpecificationRepository.java
+++ src/main/java/com/ogive/oheo/persistence/repo/ProductSpecificationRepository.java
@@ -1,11 +1,13 @@
 package com.ogive.oheo.persistence.repo;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
 
 import com.ogive.oheo.persistence.entities.ProductSpecification;
 
 public interface ProductSpecificationRepository extends CrudRepository<ProductSpecification, Long>, JpaSpecificationExecutor<ProductSpecification> {
 
+	@Modifying
 	void deleteByProductId(long productId);
 }
